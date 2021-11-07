a02ed47263040e66af8721e51024a2b11208fa45
sebastian chan <sebthemartian@sebastians-MacBook-Pro.local>
2021-11-06 19:48:09 -0700

    edit

--- tests.py
+++ tests.py
@@ -5,24 +5,25 @@ app.config['TESTING'] = True
 app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
 class TestConverter(unittest.TestCase):
-    def setUp(self):
-        tester = app.test_client(self)
 
     def test_currency_converter(self):
-        res = tester.get('/')
+        client = app.test_client(self)
+        res = client.get('/')
         html = res.get_data(as_text= True)
 
         self.assertEqual(res.status_code, 200)
         self.assertIn('<h1>Convert your money!</h1>')
 
     def test_conversion(self):
-        res = client.post('/converted',data={'currency_type': 'BTC','converted_currency' : 'BTC', 'amount': 20})
+        client = app.test_client(self)
+        res = client.post('/',data={'converting_currency': 'USD','converted_currency' : 'USD', 'amount': 20})
         html = res.get_data(as_text=True)
 
         self.assertEqual(res.status_code, 200)
-        self.assertIn('<div>฿20</div>', html)
+        self.assertIn('<div>USD$20</div>', html)
 
     def test_conversion(self):
+        client = app.test_client(self)
         res = client.post('/converted',data={'currency_type': 'MXN','converted_currency' : 'MXN', 'amount': 100})
         html = res.get_data(as_text=True)
 
